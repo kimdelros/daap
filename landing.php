c911c7dc79ab58e8914b7440c79fabea15c4108a
cruzrkent <112408510+cruzrkent@users.noreply.github.com>
2022-09-30 20:39:24 +0800

    Edit Landing Page

--- landing.php
+++ landing.php
@@ -2,58 +2,65 @@
   require_once $_SERVER['DOCUMENT_ROOT'].'/daap/resource/php/class/core/init.php';
 
  ?>
-
-<!DOCTYPE html>
+ <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
     <!-- CSS only -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
     <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
-
-    <!-- ===== Iconscout CSS ===== -->
     <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
-    <link rel="stylesheet" href="resource/css/landingStyle.css">
-    <link rel="stylesheet" href="resource/css/style3.css">
-    <title>DAAP Home Page</title>
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <link href='https://fonts.googleapis.com/css?family=Nunito' rel='stylesheet'>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="resource/css/landingstyle.css">
+    <title>DAAP Portal</title>
   </head>
   <body>
-    <div class="bg-banner">
-        <div class="navbar">
-            <div class="ceu-logo-white"><a href="https://www.ceu.edu.ph" target="_blank"><img src="resource/img/navbarLogoColorless.png" alt="ceu-logo"></a>
+    <section class="bg-banner">
+      <div class="container container-fluid">
+        <div class="row text-center">
+          <div class="daaplogo" data-aos="fade-down" data-aos-duration="2000">
+            <img src="resource/img/DAAP-transparent.png" width="230px" alt="daap-logo">
+          </div>
+          <div id="carouselWorks" class="carousel slide" data-bs-ride="carousel" data-aos="fade-up" data-aos-duration="2000">
+            <div class="carousel-inner">
+              <div class="AlumniDiscount carousel-item active container container-fluid">
+                <h2>Alumni Discount</h2>
+                <a href="#AlumniForm" class='btn btn-info btn-sm'>Apply Now</a>
+              </div>
+              <div class="SiblingDiscount carousel-item container container-fluid">
+                <h2>Sibling Discount</h2>
+                <a href="#AlumniForm" class='btn btn-info btn-sm'>Apply Now</a>
+              </div>
+              <div class="CEISDiscount carousel-item container container-fluid">
+                <h2>CEIS Discount</h2>
+                <a href="#AlumniForm" class='btn btn-info btn-sm'>Apply Now</a>
+              </div>
             </div>
 
-            <div class="social-icons">
-                <ul>
-                <li><a href="https://www.facebook.com/theCEUofficial/" target="_blank"><i class="fa fa-facebook"></i></a></li>
-                  <li><a href="https://twitter.com/centroescolaru" target="_blank"><i class="fa fa-twitter"></i></a></li>
-                  <li><a href="https://www.instagram.com/ceuofficial/" target="_blank"><i class="fa fa-instagram"></i></a></li>
-                  <li><a href="https://www.youtube.com/CEUofficial" target="_blank"><i class="fa fa-youtube"></i></a></li>
-                </ul>
-            </div>
-        </div>
-        <div class="head-tag">
-          <div class="head-tag-main">
-            <h1>Centro Escolar University</h1>
-          </div>
-          <div class="head-tag-nxt">
-            <h1>Manila - Makati - Malolos</h1>
+              <button class="carousel-control-prev" type="button" data-bs-target="#carouselWorks" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+              </button>
+              <button class="carousel-control-next" type="button" data-bs-target="#carouselWorks" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+              </button>
           </div>
         </div>
-      <div class="header-tag">
-        <img src="resource/img/landingImage.png" alt="ceu-img">
-        <h1>Student Discount Application, Verification and Alumni Portal</h1>
-        <div class="paragraph">
-          <p>Centro Escolar University offers the high-quality education and academic career that you deserve. By enrolling in CEU, you become a member of a community that is concerned with your total development intellectually, physically, emotionally, and spiritually.<br><br>CEU provides promising students like you with numerous opportunities for growth and development. Utilize the benefits we provide so that you can shape your life and future with us.</p>
-        </div>
       </div>
-      <div class="cont-btn">
-        <div class="button">
-          <a href="studentapplication.php" target="_blank" class="btn btn-1">Apply Now!</a>
-        </div>
-      </div>
-    </div>
+  </section>
+
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+  <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js" integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous"></script>
+  <script type="text/javascript" src="resources/scripts/script.js"></script>
+  <script src="https://kit.fontawesome.com/b04d2a2a76.js" crossorigin="anonymous"></script>
+  <script>
+    AOS.init();
+  </script>
   </body>
 </html>
