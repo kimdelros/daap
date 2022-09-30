265000a1a26f3c55da2a1ace9316fa6d9f4b012c
Lawrence Forbes <forbes0982@gmail.com>
2022-10-01 00:20:12 +0800

    Update landing.php

--- landing.php
+++ landing.php
@@ -27,14 +27,17 @@
             <div class="carousel-inner">
               <div class="AlumniDiscount carousel-item active container container-fluid">
                 <h2>Alumni Discount</h2>
+                <img src="resource/img/alumni.png" alt="alumni" width="600px"><br>
                 <a href="#AlumniForm" class='btn btn'></a>
               </div>
               <div class="SiblingDiscount carousel-item container container-fluid">
                 <h2>Sibling Discount</h2>
+                <img src="resource/img/sibling.png" alt="alumni" width="600px"><br>
                 <a href="#SiblingForm" class='btn btn'></a>
               </div>
               <div class="CEISDiscount carousel-item container container-fluid">
                 <h2>CEIS Discount</h2>
+                <img src="resource/img/ceis.png" alt="alumni" width="600px"><br>
                 <a href="#CEISForm" class='btn btn'></a>
               </div>
             </div>
